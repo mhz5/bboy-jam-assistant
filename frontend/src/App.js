55ce23f32f84eb167871adf6cd0aaaba32bf34aa
Mike <misc.mikez@gmail.com>
2018-09-24 22:05:27 -0700

    Added sample code to make HTTP request

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,37 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: null,
+        };
+    }
+
+    componentDidMount() {
+        fetch('https://backend-dot-bboy-jam-prod.appspot.com/test')
+            .then(response => response.text())
+            .then(data => this.setState({ data }));
+    }
+
+    render() {
+        let data = this.state.data;
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+                <p>
+                    { data }
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
