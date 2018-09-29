3a10073355d9e62e185c97652d8a62a739214bb4
Guangyi Zhou <guangyizhou@Guangyis-MBP.hsd1.ca.comcast.net>
2018-09-29 11:52:06 -0700

    Set up directory structure. Created Signup component. Installed react-router-dom. Set up root route and signup route.

---
+++ frontend/src/components/Signup/Signup.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Signup.scss';
+
+class Signup extends Component {
+
+  render() {
+    return (
+      <div className="signup">
+        This is a singup form
+      </div>
+    );
+  }
+}
+
+export default Signup;
