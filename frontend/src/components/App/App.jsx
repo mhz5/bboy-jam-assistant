a863884ed01cd297e1b0ce07156fb8bc8916dd5a
大宝贝 <zhouguangyi2009@gmail.com>
2018-09-29 11:54:43 -0700

    small fix import

--- frontend/src/components/App/App.jsx
+++ frontend/src/components/App/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from '../../logo.svg';
 import './App.css';
-import Link from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class App extends Component {
   constructor(props) {
