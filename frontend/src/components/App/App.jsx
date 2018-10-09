00c2734ab8e0a3049214f62db48a2a959736f298
Mike <misc.mikez@gmail.com>
2018-10-08 20:58:01 -0700

    remove commented lines in App.jsx

--- frontend/src/components/App/App.jsx
+++ frontend/src/components/App/App.jsx
@@ -1,23 +1,7 @@
 import React, { Component } from 'react';
-import logo from '../../logo.svg';
 import './App.scss';
 
 class App extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.API_URL = process.env.REACT_APP_API_URL;
-  //   this.state = {
-  //     data: "No data",
-  //   };
-  // }
-  //
-  // componentDidMount() {
-  //   console.log(this.API_URL);
-  //   fetch(this.API_URL + 'test')
-  //     .then(response => response.text())
-  //     .then(data => this.setState({ data }));
-  // }
-
   render() {
     return (
       <div className="home">
